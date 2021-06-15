9391e268fabdb0f641024857161905e921624f31
jinsoo <admin@binary01.me>
2021-06-15 18:21:53 +0900

    feat : config test

--- config.js
+++ config.js
@@ -1,3 +1,18 @@
 import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
 
-const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+dotenv.config()
+
+const info = {
+  accessKeyId: process.env.ID,
+  secrestAccessKey: process.env.SECRET,
+}
+
+const params = {
+  Bucket: process.env.BUCKET_NAME,
+  CreateBucketConfiguration: {
+    LocationConstraint: process.env.REGION
+  }
+}
+console.log(info, params);
+// const s3 = new AWS.S3({apiVersion: '2006-03-01'});
