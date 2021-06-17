956a35147e94f4761e74643e3fee885e8b50e1d8
jinsoo <admin@binary01.me>
2021-06-17 11:26:35 +0900

    feat : string space tag delete

--- parsing.js
+++ parsing.js
@@ -8,10 +8,11 @@ const getObject = async (key) => {
 
   const one_file = await s3.getObject(getParams).promise();
   const lineArr = one_file.Body.toString().split("\n");
-  const result = deleteNull(parsingByVerticalBar(lineArr));
+  let result = deleteNull(parsingByVerticalBar(lineArr));
 
+  ///이후 할 것들 < 만나면 >까지 지우기, \r 삭제,  { 만나면 } 삭제
+  result = deleteUnusedThings(result).filter(line => line);
   console.log(result, key);
-  ///이후 할 것들 < 만나면 >까지 지우기, \r 삭제 
 };
 
 const getAllObject = async (list) => {
@@ -29,5 +30,41 @@ const parsingByVerticalBar = (arr) => {
 };
 
 const deleteNull = (arr) => arr.filter((row) => row !== null);
+
+const deleteUnusedThings = (arr) => {
+  const blankPattern = /^\s+|\s+$/g;
+
+  let parsedResult = arr.map((line) => {
+    let newLine = "";
+    let flag = false;
+    line = line.replaceAll("\r", "");
+    for (let i = 0; i < line.length; i++) {
+      if (line[i] === "<") {
+        flag = true;
+        continue;
+      }
+      if (line[i] === ">") {
+        flag = false;
+        continue;
+      }
+      if (!flag) newLine += line[i];
+    }
+    newLine = newLine.replaceAll("&nbsp;", "");
+    newLine = newLine.replace(/ +/g, " ");
+    if(newLine.replace(blankPattern, '') === '') return null;
+    return newLine;
+  });
+
+  return parsedResult;
+};
+
+const checkKor = (str) => {
+  const regExp = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$/;
+  if (regExp.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 export { getAllObject, getObject };
